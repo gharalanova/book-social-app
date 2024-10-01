0d4f68e8e27e670dd15dc18760e80b3f3b6a2311
Gabriela Ivanova <slynchoO@users.noreply.github.com>
2024-10-01 13:03:59 +0300

    impl the user details service

---
+++ src/main/java/com/projectgaby/book_social_app/security/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.projectgaby.book_social_app.security;
+
+import com.projectgaby.book_social_app.user.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    //where we will load our user from ? - database
+
+    private final UserRepository repository;
+
+    @Override
+    @Transactional // I want to load the user and the roles with it
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        return repository.findByEmail(userEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
